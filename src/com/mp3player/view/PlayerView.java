30b28972720eb4d65c2c85381b9bd172aadbdc99
mv-stns <21103251+mv-stns@users.noreply.github.com>
2024-12-16 12:25:28 +0100

    Refactor PlayerView class to inject MP3Player dependency

--- src/com/mp3player/view/PlayerView.java
+++ src/com/mp3player/view/PlayerView.java
@@ -1,27 +1,23 @@
 package com.mp3player.view;
 
+import com.mp3player.model.MP3Player;
 import com.mp3player.view.components.Controls;
 import com.mp3player.view.components.SongInfo;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 
 public class PlayerView extends BorderPane {
 
     private SongInfo songInfo;
     private Controls controls;
+    private MP3Player player;
     VBox mainPane;
 
-    public PlayerView() {
+    public PlayerView(MP3Player player) {
+        this.player = player;
         controls = new Controls();
         mainPane = new VBox(12);
-        songInfo = new SongInfo();
+        songInfo = new SongInfo(player);
         this.setCenter(mainPane);
         mainPane.getChildren().addAll(songInfo, controls);
         mainPane.setStyle("-fx-background-radius: 10;");
